fb503dac3b30e3e2ca9c5e5d6bcf2cc0b5db3285
Stefan Holmberg <stefan.holmberg@systementor.se>
2020-09-15 13:24:28 +0200

    Add project files.

---
+++ Cms20_OOpLab2/Person.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cms20_OOpLab2
+{
+    class Person
+    {
+        public string Name { get; private set; }
+        public string StreetAddress { get; private set; }
+        public int PostalCode { get; private set; }
+        public string City { get; private set; }
+
+        public DateTime BirthDate { get; private set; }
+
+        public Person(DateTime birthDate)
+        {
+            BirthDate = birthDate;
+        }
+
+        private bool IsOkName(string newName)
+        {
+            if (newName == "IDIOT") return false;
+            if (newName == "HEJ") return false;
+            if (newName == "HOPP") return false;
+            if (newName == "JHKJKL") return false;
+            if (newName.Length < 2) return false;
+            return true;
+        }
+
+        public bool SetName(string newName)
+        {
+
+            if (IsOkName(newName) == false) return false;
+            Name = newName;
+            return true;
+        }
+
+        public void SendDiscountOnLocalIca()
+        {
+            //SendPost(  namer, streeaddress )
+        }
+
+        public void SendFlowers()
+        {
+            //SendMail("interfloa.se", "Grattis -> klevera" )
+        }
+
+
+
+        public void SetAddress(string streetAddress, int postalCode
+            , string city)
+        {
+            StreetAddress = streetAddress;
+            PostalCode = postalCode;
+            City = city;
+            SendDiscountOnLocalIca();
+        }
+
+        public void BecomeSamboWith(Person other)
+        {
+            StreetAddress = other.StreetAddress;
+            PostalCode = other.PostalCode;
+            City = other.City;
+
+            SendFlowers();
+        }
+
+    }
+}
---
+++ Cms20_OOpLab2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Cms20_OOpLab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(new DateTime(2012,1,1));
+
+            person.BBla();
+
+
+            if (person.SetName("IDIOT") == false)
+            {
+                Console.WriteLine("Ogiltigt namn");
+            }
+
+            person.SetName("Stefan");
+            person.SetAddress("Blavägen 1", 11122, "Hejstad");
+
+
+
+
+            var kerstin = new Person(new DateTime(2012, 1, 1));
+            kerstin.SetName("Kerstin");
+            kerstin.SetAddress("Hejgartan 111", 55511, "Blastad");
+
+            person.BecomeSamboWith(kerstin);
+
+        }
+    }
+}
